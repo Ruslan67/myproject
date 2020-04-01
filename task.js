4a991490f1e0a3a5238ab8e9d1362938f0fc629b
Ruslan <rparfiriev@yahoo.de>
2020-04-01 10:52:53 +0300

    5 commit

--- task.js
+++ task.js
@@ -5,9 +5,12 @@ function sent(event) {
     const kod = document.getElementById("kod")
     const weight = document.getElementById("weight")
     console.log(text1.value, kod.value, weight.value);
-    const ekran = document.getElementById("ekran")
-    ekran.innerText = (text1.value + kod.value + weight.value)
-
+    //const ekran = document.getElementById("ekran")
+    //ekran.innerText = (text1.value + kod.value + weight.value)
+    const newDiv = document.createElement("div");
+    const container = document.getElementById("container")
+    container.appendChild(newDiv);
+    newDiv.textContent = text1 + ' ' + kod + ' ' + weight;
 
 
 
